07f6b85a767a6d2f9927d127b92ffdfb45e8e832
liaochongrong <chongrong.liao@urovo.com>
2023-04-11 14:20:19 +0800

    本地化

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      name: 'User',
+      description: '点击跳转 <a href="https://icu007.work" class="text-blue-500" target="_blank" >主站</a>',
     },
   }
 }
